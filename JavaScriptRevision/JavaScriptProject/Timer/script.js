b2728ca76a50368ed916d5b96df3aad42cfb1061
ksushantt72 <ksushantt72@gmail.com>
2025-12-29 23:34:49 +0530

    some files moved to Java-script repository

--- JavaScriptRevision/JavaScriptProject/Timer/script.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('input-box');
-
-//we will get the value from the boc
-const button = document.getElementById('btn');
-button.addEventListener('click', () => {
-    console.log(input.value)
-});
-
-// now we will apply interval
