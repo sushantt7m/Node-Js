8804932789208dcca7838c2e39a4963fd7518b4d
ksushantt72 <ksushantt72@gmail.com>
2026-03-01 19:47:41 +0530

    dont know

---
+++ 06URLHandling/index.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url')
+
+
+const myServer = http.createServer((req, res) => {
+    if (req.url === '/favicon.ico') return res.end();
+    const log = `${Date.now()}:${req.url}  Request Received\n`
+
+    const myUrl = url.parse(req.url, true)
+    console.log(myUrl);
+
+    fs.appendFile('./log.txt', log, (err, data) => {
+        switch (myUrl.pathname) {
+            case '/': res.end("Home Page");
+                break;
+            case '/about':
+                const username = myUrl.query.myName
+                const userId = myUrl.query.userId
+
+                res.end(`Username is : ${username} and userId is :${userId}`);
+
+                break;
+            case '/contact': res.end("contact");
+                break;
+            default: res.end("404 Not found");
+        }
+    })
+})
+
+myServer.listen("8000", console.log("Server Started"));
